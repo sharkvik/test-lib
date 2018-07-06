ece7b932b513e09b4e969fc2e8b96c441f868379
Виктор Фамилия <shark_vik@inbox.ru>
2018-07-06 22:48:15 +0300

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TestLibModule} from 'test-lib';
-import { EmptyComponent } from './app/empty/empty.component';
 
 const routes: Routes = [
-  { path: 'test', loadChildren: 'test-lib/bundles/test-lib.umd.min.js#TestLibModule' },
-  { path: '', component: EmptyComponent },
-  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
