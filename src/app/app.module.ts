0aa92fedd1cb7254a8ae4fddf5ad2b6baf5ecebb
Viktor <shark_vik@inbox.ru>
2018-07-10 23:48:15 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestLibModule } from 'test-lib';
-import {NvmLoaderModule} from 'nvm-loader';
+import { NvmLoaderModule } from 'nvm-loader';
 
 @NgModule({
   declarations: [
